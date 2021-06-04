1704258e75ae7a26ae4f8edcf6408f05df0b0df7
nuno-21902803 <nunomaluko5@gmail.com>
2021-06-04 16:46:25 +0100

    update

--- website/urls.py
+++ website/urls.py
@@ -1,14 +1,43 @@
 #  website/urls.py
-
-from django.urls import path
 from django.shortcuts import render
+from django.urls import path
 from . import views
-
 
 app_name = "website"
 
 urlpatterns = [
-    path('', views.home_page_view, name='home'),
-    path('details/<int:productId>', views.details_page_view, name='details'),
-    path('contacts', views.contacts_page_view, name='contacts')
+    path('', views.index_page_view, name='index'),
+    path('ligaEscocesa/', views.liga_escocesa_view, name='ligaEscocesa'),
+    path('PremierLeague/', views.premierLeague_view, name='PremierLeague'),
+    path('about/', views.about_view, name="about"),
+    path('aberdeen/', views.aberdeen_view, name='aberdeen'),
+    path('arsenal/', views.arsenal_view, name='arsenal'),
+    path('astonVilla/', views.aston_villa_view, name='astonVilla'),
+    path('burnley/', views.burnley_view, name='burnley'),
+    path('celtic/', views.celtic_view, name='celtic'),
+    path('chelsea/', views.chelsea_view, name='chelsea'),
+    path('critica/', views.critica_view, name='critica'),
+    path('crystalPalace/', views.crystal_palace_view, name='crystalPalace'),
+    path('everton/', views.everton_view, name='everton'),
+    path('fulham/', views.fulham_view, name='fulham'),
+    path('hiberian/', views.hiberian_view, name='hiberian'),
+    path('info/', views.info_view, name='info'),
+    path('leeds/', views.leeds_view, name='leeds'),
+    path('leicester/', views.leicester_view, name='leicester'),
+    path('link/', views.links_view, name='link'),
+    path('liverpool/', views.liverpool_view, name='liverpool'),
+    path('livingston/', views.livingston_view, name='livingston'),
+    path('manCity/', views.manCity_view, name='manCity'),
+    path('manUnited/', views.manUnited_view, name='manUnited'),
+    path('newcastle/', views.newcastle_view, name='newcastle'),
+    path('rangers/', views.rangers_view, name='rangers'),
+    path('rossCountry/', views.rossCountry_view, name='rossCountry'),
+    path('sheffield/', views.sheffield_view, name='sheffield'),
+    path('shoutampton/', views.southampton_view, name='southampton'),
+    path('spurs/', views.spurs_view, name='spurs'),
+    path('westBromwich/', views.west_bromwich_view, name='westBromwich'),
+    path('westHam/', views.westHam_view, name='westHam'),
+    path('wolves/', views.wolves_view, name='wolves'),
+    path('wordcloud/', views.wordcloud_view, name='wordcloud'),
+    path('brigton/', views.brighton_view, name='brighton'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,18 +1,139 @@
 #  config/views.py
-
+from django.http import HttpResponse
 from django.shortcuts import render
-from datetime import datetime
 
 
-def home_page_view(request):
-    context = {'now': datetime.now()}
-    return render(request, 'website/home.html', context)
+def index_page_view(request):
+    return render(request, "website/index.html")
 
 
-def details_page_view(request, productId):
-    #Query to search product with the path parameter
-    return render(request, 'website/details.html', {'productId': productId})
+def liga_escocesa_view(request):
+    return render(request, "website/ligaEscocesa.html")
 
 
-def contacts_page_view(request):
-    return render(request, 'website/contacts.html')
+def aberdeen_view(request):
+    return render(request, "website/aberdeen.html")
+
+
+def about_view(request):
+    return render(request, "website/about.html")
+
+
+def arsenal_view(request):
+    return render(request, "website/arsenal.html")
+
+
+def aston_villa_view(request):
+    return render(request, "website/aston-villa.html")
+
+
+def brighton_view(request):
+    return render(request, "website/brigton.html")
+
+
+def burnley_view(request):
+    return render(request, "website/Burnley.html")
+
+
+def celtic_view(request):
+    return render(request, "website/celtic.html")
+
+
+def chelsea_view(request):
+    return render(request, "website/chelsea.html")
+
+
+def critica_view(request):
+    return render(request, "website/critica.html")
+
+
+def crystal_palace_view(request):
+    return render(request, "website/Crystal-Palace.html")
+
+
+def everton_view(request):
+    return render(request, "website/everton.html")
+
+
+def fulham_view(request):
+    return render(request, "website/Fulham.html")
+
+
+def hiberian_view(request):
+    return render(request, "website/hibernian.html")
+
+
+def info_view(request):
+    return render(request, "website/info.html")
+
+
+def leeds_view(request):
+    return render(request, "website/leeds.html")
+
+
+def leicester_view(request):
+    return render(request, "website/leicester.html")
+
+
+def links_view(request):
+    return render(request, "website/links.html")
+
+
+def liverpool_view(request):
+    return render(request, "website/liverpool.html")
+
+
+def livingston_view(request):
+    return render(request, "website/livingston.html")
+
+
+def manCity_view(request):
+    return render(request, "website/manCity.html")
+
+
+def manUnited_view(request):
+    return render(request, "website/manUnited.html")
+
+
+def newcastle_view(request):
+    return render(request, "website/Newcastle.html")
+
+
+def premierLeague_view(request):
+    return render(request, "website/premierLeague.html")
+
+
+def rangers_view(request):
+    return render(request, "website/rangers.html")
+
+
+def rossCountry_view(request):
+    return render(request, "website/rossCounty.html")
+
+
+def sheffield_view(request):
+    return render(request, "website/Sheffield.html")
+
+
+def southampton_view(request):
+    return render(request, "website/southampton.html")
+
+
+def spurs_view(request):
+    return render(request, "website/spurs.html")
+
+
+def west_bromwich_view(request):
+    return render(request, "website/West-Bromwich.html")
+
+
+def wolves_view(request):
+    return render(request, "website/wolves.html")
+
+
+def wordcloud_view(request):
+    return render(request, "website/wordcloud.html")
+
+
+def westHam_view(request):
+    return render(request, "website/west-ham.html")
